4b66f8bf5da8b0b0a223f926d32c6d11d69b4c33
Davis Fe <davisfe@email.cz>
2025-05-09 23:54:46 +0200

    refactoring

--- apiObjects/jsonPlaceHolder/jsonPlaceHolderApi.service.ts
+++ apiObjects/jsonPlaceHolder/jsonPlaceHolderApi.service.ts
@@ -1,23 +1,22 @@
 import {ApiService} from "../api.service.ts";
 import {JsonPlaceHolderUrl} from "./jsonPlaceHolderUrl.ts";
 
-export class JsonPlaceHolderApiService {
-     api = new ApiService();
+export class JsonPlaceHolderApiService extends ApiService{
      url = new JsonPlaceHolderUrl();
 
     public async getPosts(postId: string) {
-        return this.api.GET(await this.url.getPostsUrl(postId));
+        return this.GET(await this.url.getPostsUrl(postId));
     }
 
     public async postPosts(body: object) {
-        return this.api.POST(await this.url.postPostsUrl(), body);
+        return this.POST(await this.url.postPostsUrl(), body);
     }
 
     public async putPosts(postId: string, body: object) {
-        return this.api.PUT(await this.url.putPostsUrl(postId), body);
+        return this.PUT(await this.url.putPostsUrl(postId), body);
     }
 
     public async deletePosts(postId: string) {
-        return this.api.DELETE(await this.url.putPostsUrl(postId));
+        return this.DELETE(await this.url.putPostsUrl(postId));
     }
 }
